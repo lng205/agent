635093512635d03f0c6b3c106090fdcbf5c72c68
lng205 <youbingyoubing@gmail.com>
2024-01-27 10:19:44 +0800

    init

---
+++ chat.py
@@ -0,0 +1,46 @@
+from openai import OpenAI
+client = OpenAI()
+import chromadb, os
+chroma_client = chromadb.Client()
+
+import chromadb.utils.embedding_functions as embedding_functions
+openai_ef = embedding_functions.OpenAIEmbeddingFunction(
+                api_key=os.environ["OPENAI_API_KEY"],
+                model_name="text-embedding-3-small"
+            )
+collection = chroma_client.create_collection(name="my_collection", embedding_function=openai_ef)
+
+collection.add(
+    documents=["主人提到他早上更喜欢喝咖啡而不是茶。",
+    "主人上周四晚上表达了对爵士音乐的喜爱。",
+    "主人在周三的对话中透露他不喜欢寒冷的天气。",
+    "主人在昨天的聊天中分享了一次难忘的登山故事。",
+    "主人表示，科幻是他最喜欢的电影类型。",
+    "这周一厨房的架子上出现了一套新的绿色陶瓷盆。",
+    "周二下午注意到客厅的地毯上有一个新的污渍。",
+    "周末主人为办公室角落带来了一盆大而叶茂的植物。",
+    "自上周五以来，走廊桌上积累了一堆未读的邮件。",
+    "昨天，餐厅区域的墙壁被重新粉刷成浅薰衣草色。",
+    "主人在周六早上接到一个电话后显得特别开心。",
+    "主人这个周日完成了书房里一个木质书架的组装。",
+    "今天早餐时，主人对一件故障的厨房用具显得很沮丧。",
+    "观察到主人为下周末的家庭聚会练习吉他。",
+    "主人昨晚花时间整理旧照片进相册。"],
+    ids=["1", "2", "3", "4", "5", "6", "7", "8", "9", "10","11","12","13","14","15"]
+)
+
+results = collection.query(
+    query_texts=["主人喜欢的电影是什么样的？"],
+    n_results=3
+)
+
+print(results)
+
+# system_message = "You shoule give reply based on these relavant information:"
+
+def chat(user_message):
+    completion = client.chat.completions.create(
+    model="gpt-4-0125-preview",
+    messages=[{"role": "user", "content": user_message}])
+
+    print(completion)
